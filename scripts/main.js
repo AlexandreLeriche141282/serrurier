7e91c4ec3c3d8df6fd2681a0c8572a6f552c96d5
alexandreleriche141282 <alexandreleriche141282@gmail.com>
2025-04-05 20:01:51 +0200

    first version desktop

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,22 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Sélectionner le bouton hamburger et le menu de navigation
-    const hamburger = document.querySelector('.hamburger');
-    const navLinks = document.querySelector('.nav-links');
+// Sélectionner le bouton
+const backToTopBtn = document.getElementById("backToTopBtn");
 
-    if (hamburger && navLinks) {
-        // Ajouter un écouteur d'événement de clic au bouton hamburger
-        hamburger.addEventListener('click', function () {
-            navLinks.classList.toggle('active'); // Basculer la classe 'active'
-        });
-
-        // Fermer le menu lorsqu'un lien est cliqué (optionnel)
-        const navItems = document.querySelectorAll('.nav-links li a');
-        navItems.forEach(item => {
-            item.addEventListener('click', function () {
-                navLinks.classList.remove('active');
-            });
-        });
+// Fonction pour afficher ou cacher le bouton selon le défilement
+window.onscroll = function () {
+    if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 550) {
+        backToTopBtn.style.display = "block"; // Montrer le bouton
     } else {
-        console.error('Élément(s) manquant(s) dans le DOM.');
+        backToTopBtn.style.display = "none"; // Cacher le bouton
     }
+};
+
+// Fonction pour remonter en haut de la page
+backToTopBtn.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth", // Défilement fluide
+    });
 });
+
 
 
