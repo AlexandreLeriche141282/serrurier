e9ed25ab54a62edebd43f3a318f964d179c42f6b
AlexandreLeriche141282 <139038579+AlexandreLeriche141282@users.noreply.github.com>
2025-07-10 19:19:14 +0200

    add section faq

--- scripts/main.js
+++ scripts/main.js
@@ -37,3 +37,13 @@ document.querySelectorAll('img').forEach(img => {
   }
 });
 
+
+  document.querySelectorAll('.accordion').forEach(btn =>
+    btn.addEventListener('click', () => {
+      btn.classList.toggle('active');
+      const panel = btn.nextElementSibling;
+      panel.style.maxHeight = panel.style.maxHeight
+        ? null
+        : panel.scrollHeight + 'px';
+    })
+  );
